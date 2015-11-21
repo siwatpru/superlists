51d838b0c1e2eacc689514faf4b35f8f3159f77b
Siwat Pruksapanya <siwat.pru@outlook.com>
2015-11-21 18:53:10 +0700

    Adjust new list view to do model validation

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 
 from .models import Item, List
@@ -17,5 +18,12 @@ def add_item(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {'error': error})
     return redirect('/lists/%d/' % list_.id)
